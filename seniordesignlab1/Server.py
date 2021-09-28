5caf79b97faed4bd33f8e08107819e6461d70c7f
Steve Wasiswa <steve-wasiswa@uiowa.edu>
2021-09-28 11:36:19 -0500

    Added a remote button

--- seniordesignlab1/Server.py
+++ seniordesignlab1/Server.py
@@ -10,20 +10,30 @@ PORT = 3000
 
 print("Starting server, port number is " + str(PORT))
 
+
 def insert(msg):
-	db.insert({'message': msg})
+    db.insert({'message': msg})
+
 
 async def echo(websocket, path):
     print("a client just connected")
     try:
         async for message in websocket:
-            print("Recieved message from client: " + message)
-            insert(message)
-            await websocket.send("Data Recieved")
+            print("Received message from client: " + message)
+            """If message is within websocket is lightUpScreen, then send it to Arduino; 
+            otherwise, add it to the database"""
+            if message == 'false' or message == 'true':
+                print(message)
+                websocket.send(message)
+            else:
+                insert(message)
+            await websocket.send("Data Received")
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
 
-server = websockets.serve(echo, "192.168.0.64", PORT)
+    localserver = "192.168.0.64"
+
+server = websockets.serve(echo, "192.168.0.202", PORT)
 
 asyncio.get_event_loop().run_until_complete(server)
 asyncio.get_event_loop().run_forever()
