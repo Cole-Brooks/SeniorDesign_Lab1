11cc835def39958a0cb0c13db7e10e20f69ef1d6
Cole Brooks <cole.brooks@hotmail.com>
2021-09-30 22:24:17 -0500

    added conversions to charts

--- seniordesignlab1/public/app.js
+++ seniordesignlab1/public/app.js
@@ -1,4 +1,5 @@
-var y = new Array(300).fill(null);
+var y_C = new Array(300).fill(null);
+var y_F = new Array(300).fill(null);
 var x = Array.from(Array(300 + 1).keys()).slice(1);
   
   //CELSIUS
@@ -9,7 +10,7 @@ var x = Array.from(Array(300 + 1).keys()).slice(1);
     labels: x.reverse(),
     datasets: [{
       label: 'Temperature Data in Celsius',
-      data: y,
+      data: y_C,
       backgroundColor: 'rgba(255, 26, 104, 0.2)',
       borderColor: 'rgba(255, 26, 104, 1)',
       tension: 0.5
@@ -18,107 +19,122 @@ var x = Array.from(Array(300 + 1).keys()).slice(1);
 
   // config 
   const config_C = {
-    type: 'line',
-    data: data1,
-    options: {
-      scales: {
-        y: {
-          min: -20,
-          max: 50,
-        },
-        x:{
-          min: 0,
-          //max: 300,
-          ticks: { 
-            maxTicksLimit: 6
-          }
-        }
-      }, 
-      plugins: {
-          zoom: {
-              limits:{
-                  y: {min: -200, max: 200}
-              },
-              pan:{
-                  enabled: true,
-                  mode: 'y',
-                  limits:{
-                    y:{min:-200, max:200}
-                  }
-              },
+        type: 'line',
+        data: data1,
+        options: {
+          scales: {
+            y: {
+              min: 10,
+              max: 50,
+            },
+            x:{
+              min: 0,
+              //max: 300,
+              ticks: { 
+                reverse: true,
+                maxTicksLimit: 6,
+                stepSize: 50
+              }
+            }
+          }, 
+          plugins: {
               zoom: {
-                  mode: 'y',
-                  wheel: {
-                      enabled: true,
+                  limits:{
+                      y: {min: 10, max: 50}
                   },
-                  drag: {
-                    enabled: false,
+                  pan:{
+                      enabled: true,
+                      mode: 'y',
+                      limits:{
+                        y:{min: 10, max: 50}
+                      }
+                  },
+                  zoom: {
+                      mode: 'y',
+                      wheel: {
+                          enabled: true,
+                      },
+                      drag: {
+                        enabled: false,
+                      }
+    
                   }
-
               }
           }
-      }
-    }
-  };
-
-  // render init block
-  const myChart1 = new Chart(
-    document.getElementById('myChart1'),
-    config_C
-  );
-
-  
-  //FARENHEIGHT
-  // setup 
-  const data2 = {
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun',
-      'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun',
-      'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      datasets: [{
-        label: 'Temperature Data in Farenheight',
-        data: [100, 115, 72, 68, 77, 82, 99, 100, 115, 72, 68, 77, 82, 99, 100, 115, 72, 68, 77, 82, 99, 100, 115, 72, 68, 77, 82, 99,
-          100, 115, 72, 68, 77, 82, 99, 100, 115, 72, 68, 77, 82, 99, 100, 115, 72, 68, 77, 82, 99, 100, 115, 72, 68, 77, 82, 99,],
-        backgroundColor: 'rgba(255, 26, 104, 0.2)',
-        borderColor: 'rgba(255, 26, 104, 1)',
-        tension: 0.5
-      }]
-    };
-
-    // config 
-    const config_F = {
-      type: 'line',
-      data: data2,
-      options: {
-        scales: {
-          y: {
-            min: 50,
-            max: 122,
-          }
-        }, 
-        plugins: {
-            zoom: {
-                limits:{
-                    y: {min: 50, max: 122}
-                },
-                pan:{
-                    enabled: true,
-                    mode: 'xy'
-                },
-                zoom: {
-                    wheel: {
-                        enabled: true,
-                    }
-                }
-            }
         }
-      }
-    };
-
-    // render init block
-    const myChart2 = new Chart(
-      document.getElementById('myChart2'),
-      config_F
-    );
+      };
+  
+      // render init block
+      const myChart1 = new Chart(
+        document.getElementById('myChart1'),
+        config_C
+      );
+  
+      
+      //FARENHEIGHT
+      // setup 
+      const data2 = {
+        labels: x,
+        datasets: [{
+          label: 'Temperature Data in Fahrenheit',
+          data: y_F,
+          backgroundColor: 'rgba(255, 26, 104, 0.2)',
+          borderColor: 'rgba(255, 26, 104, 1)',
+          tension: 0.5
+        }]
+      };
+    
+        // config 
+        const config_F = {
+          type: 'line',
+          data: data2,
+          options: {
+            scales: {
+              y: {
+                min: 50,
+                max: 122,
+              },
+              x:{
+                min: 0,
+                //max: 300,
+                ticks: { 
+                  reverse: true,
+                  maxTicksLimit: 6
+                }
+              }
+            }, 
+            plugins: {
+              zoom: {
+                  limits:{
+                      y: {min: 50, max: 122}
+                  },
+                  pan:{
+                      enabled: true,
+                      mode: 'y',
+                      limits:{
+                        y:{min: 50, max: 122}
+                      }
+                  },
+                  zoom: {
+                      mode: 'y',
+                      wheel: {
+                          enabled: true,
+                      },
+                      drag: {
+                        enabled: false,
+                      }
+    
+                  }
+              }
+          }
+          }
+        };
+    
+        // render init block
+        const myChart2 = new Chart(
+          document.getElementById('myChart2'),
+          config_F
+        );
 
 
   function toFarenheight(){
@@ -152,8 +168,13 @@ document.addEventListener('DOMContentLoaded', function() {
         PrevTemps.once('value', function(snap){
           snap.forEach(function(item){
             var itemVal = item.val();
-            y.shift();
-            y.push(itemVal);
+            y_C.shift();
+            y_C.push(itemVal);
+            
+            itemVal = (1.8 * itemVal) + 32;
+            y_F.shift();
+            y_F.push(itemVal);          
+
           });
         });
 
@@ -175,9 +196,14 @@ document.addEventListener('DOMContentLoaded', function() {
               has_alerted = false;
               console.log("Time:" + cur_time);
               console.log("Temp:" + cur_temp);
-              y.shift();
-              y.push(cur_temp);
+              y_C.shift();
+              y_C.push(cur_temp);
               myChart1.update();
+
+              cur_temp = (1.8 * cur_temp) + 32;
+              y_F.shift();
+              y_F.push(cur_temp);
+              myChart2.update();
             }
         });
 
